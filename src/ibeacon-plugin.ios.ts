c3e10fe2def2b0ccfec884ba989bb1a70bda73e5
ketan <ketan@prioriti.com>
2018-03-13 17:56:01 +0530

    ios ts file

--- src/ibeacon-plugin.ios.ts
+++ src/ibeacon-plugin.ios.ts
@@ -1,5 +1,46 @@
-import { Common } from './ibeacon-plugin.common';
 
-export class IbeaconPlugin extends Common {
+interface ESTDeviceManagerDelegate extends NSObjectProtocol {
 
+	deviceManagerDidDiscoverDevices?(manager: ESTDeviceManager, devices: NSArray<ESTDevice>): void;
+
+	deviceManagerDidFailDiscovery?(manager: ESTDeviceManager): void;
+
+	deviceManagerDidFailDiscoveryWithError?(manager: ESTDeviceManager, error: NSError): void;
+}
+
+export class ESTDeviceManagerDelegateImpl extends NSObject implements ESTDeviceManagerDelegate{
+
+
+    static ObjCProtocols = [ESTDeviceManagerDelegate]
+
+    static new(): ESTDeviceManagerDelegateImpl{
+        return <ESTDeviceManagerDelegateImpl>super.new()
+    }
+
+    deviceManagerDidDiscoverDevices(manager, devices){
+        console.log(devices)
+    }
+
+    deviceManagerDidFailDiscovery(manager){
+        console.log('did fail ?')
+    }
+
+    deviceManagerDidFailDiscoveryWithError(manager, error){
+        console.log('error')
+    }
+
+
+}
+
+export class Bescon{
+    public manager: ESTDeviceManager ;
+
+    delegate: ESTDeviceManagerDelegateImpl = ESTDeviceManagerDelegateImpl.new()
+
+    constructor(){
+        this.manager = ESTDeviceManager.alloc()
+        this.manager.delegate = this.delegate
+    }
+    
+    
 }
